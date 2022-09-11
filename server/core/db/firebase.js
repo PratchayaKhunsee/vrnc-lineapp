0f5e56a9c6a91137fe2ac753bc6a44448c4e77fa
realpratchayakhunsee <pratchaya.41.3943@gmail.com>
2022-09-11 11:18:05 +0700

    Fix: rely on old traditional method

--- server/core/db/firebase.js
+++ server/core/db/firebase.js
@@ -72,9 +72,16 @@ async function select(refPath, ...matches) {
     if (matches.length == 0) return await get(ref(db, refPath)).then(snapshot => snapshot.val());
 
     const list = [];
-    const queryParams = [ref(db, refPath), orderByKey()];
-    Array.prototype.push.apply(list, matches.map(m => equalTo(m.value, m.key)));
-    (await get(query.apply(null, queryParams))).forEach(snapshot => list.push(snapshot.val()));
+
+    (await get(query(ref(db, refPath), orderByKey()))).forEach(snapshot => {
+        for (let m of matches) {
+            let instance = snapshot.val();
+
+            if(instance[m.key] === m.value) continue;
+            list.push(instance);
+            return;
+        }
+    });
 
     return list;
 }
