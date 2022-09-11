f814dd227018a3836bd554d9ea85247f0bba6c52
realpratchayakhunsee <pratchaya.41.3943@gmail.com>
2022-09-11 12:29:32 +0700

    Inspecting

--- client/js/vaccination/history.js
+++ client/js/vaccination/history.js
@@ -10,17 +10,31 @@
         const main = getTemplate('main').content.cloneNode(true);
         const listItem = getTemplate('list-item');
 
-        document.querySelector('main').appendChild(main);
+        
 
         try {
             let res = await GET('/vaccination/list');
             let vaccination = await res.json();
+            /** @type {HTMLDivElement} */
+            let firstChild;
+
+            for(let o of main.childNodes){
+                if(o instanceof HTMLDivElement){
+                    firstChild = o;
+                    break;
+                }
+            }
 
             for (let n of vaccination) {
                 let item = listItem.content.cloneNode(true);
-                console.log({item});
-                main.firstChild.appendChild(item);
+                console.log(vaccination);
+
+                firstChild.appendChild(item);
+                
+                // main.childNodes.item.appendChild(item);
             }
+
+            document.querySelector('main').appendChild(main);
         } catch (error) {
             return;
         }
