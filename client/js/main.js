34f2e7baa9e8598b6af94c9841fe3f768c70d87f
Pratchaya Khunsee <pratchaya.41.3943@gmail.com>
2024-01-21 03:26:28 +0700

    More update liff app

--- client/js/main.js
+++ client/js/main.js
@@ -3,7 +3,6 @@
  * @param {*} body 
  */
 async function authenticate(body = null) {
-    await initLiff();
     const auth = getAuthToken();
     const headers = {
         'content-type': 'application/x-www-from-urlencoded',
@@ -27,28 +26,11 @@ async function authenticate(body = null) {
 }
 
 /**
- * ใช้งาน Line LIFF API 
- * 
- * @returns {Promise<string>}
- */
-async function initLiff() {
-    try {
-        if(!liff.isInClient()) return;
-
-        await liff.init({ liffId: '1656071963-6OqLKl7G' });
-    } catch (error) {
-        console.error(error);
-    }
-}
-
-/**
  * รับค่าโทเค่น
  * @returns {string?}
  */
 async function getAuthToken(){
-    if(!liff.isInClient()) return localStorage.getItem('authorization');
-
-    return liff.getIDToken();
+     return localStorage.getItem('authorization');
 }
 
 /**
